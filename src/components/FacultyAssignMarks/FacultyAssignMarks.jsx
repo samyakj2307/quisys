b790446660dca9110268c1af1b58457a9b1bc13c
Samyak Jain <jainsamyak2300@gmail.com>
2021-05-31 22:07:33 +0530

    final commit

--- src/components/FacultyAssignMarks/FacultyAssignMarks.jsx
+++ src/components/FacultyAssignMarks/FacultyAssignMarks.jsx
@@ -1,97 +1,92 @@
 import "./FacultyAssignMarks.css";
-import React, {useContext, useState} from "react";
-import {Col, Row} from "react-bootstrap";
+import React, { useContext, useState } from "react";
+import { Col, Row } from "react-bootstrap";
 import FacultyAssignRow from "./FacultyAssignRow";
-import {useHistory} from "react-router-dom";
-import {ExamStudentListContext} from "../../context/ExamStudentListContext";
+import { useHistory } from "react-router-dom";
+import { ExamStudentListContext } from "../../context/ExamStudentListContext";
 
 const axios = require("axios").default;
 
 const baseUrl = "http://localhost:3000";
 
 function FacultyAssignMarks() {
-    const history = useHistory();
-    const user = history.location.state.user;
-    const marksList = history.location.state.student;
-    const index = history.location.state.index;
+  const history = useHistory();
+  const user = history.location.state.user;
+  const marksList = history.location.state.student;
+  const index = history.location.state.index;
 
-    const [examStudentDetails, setExamStudentDetails] = useContext(ExamStudentListContext)
+  const [examStudentDetails, setExamStudentDetails] = useContext(
+    ExamStudentListContext
+  );
 
-    const [totalMarks, setTotalMarks] = useState(marksList.totalMarks)
+  const [totalMarks, setTotalMarks] = useState(marksList.totalMarks);
 
-    function handleMarksChange(value, index) {
-        marksList.studentAnswerSheet[index]["marksAwarded"] = value;
-        let total = 0
-        for (let k = 0; k < marksList.studentAnswerSheet.length; k++) {
-            if (marksList.studentAnswerSheet[k].marksAwarded !== undefined) {
-                total = total + parseInt(marksList.studentAnswerSheet[k].marksAwarded);
-            }
-        }
-
-        setTotalMarks(total)
+  function handleMarksChange(value, index) {
+    marksList.studentAnswerSheet[index]["marksAwarded"] = value;
+    let total = 0;
+    for (let k = 0; k < marksList.studentAnswerSheet.length; k++) {
+      if (marksList.studentAnswerSheet[k].marksAwarded !== undefined) {
+        total = total + parseInt(marksList.studentAnswerSheet[k].marksAwarded);
+      }
     }
 
-    function handleSaveMarks() {
-        console.log(marksList)
-        marksList["totalMarks"] = totalMarks
-        axios
-            .post(baseUrl + "/verifiedResponses", marksList)
-            .then(function (response) {
-                console.log(response.data)
-                setExamStudentDetails((prevState) => {
-                    prevState[index] = marksList
-                    return prevState
-                })
-                history.push("/ExamStudentList");
+    setTotalMarks(total);
+  }
 
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
-    }
+  function handleSaveMarks() {
+    console.log(marksList);
+    marksList["totalMarks"] = totalMarks;
+    axios
+      .post(baseUrl + "/verifiedResponses", marksList)
+      .then(function (response) {
+        console.log(response.data);
+        setExamStudentDetails((prevState) => {
+          prevState[index] = marksList;
+          return prevState;
+        });
+        history.push("/ExamStudentList");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
+  return (
+    <div className={"container"}>
+      <Row className={"col1 col3"} style={{ textAlign: "center" }}>
+        <Col>
+          <h3>{marksList.studentName}</h3>
+        </Col>
+        <Col>
+          <h3>{marksList.examName}</h3>
+        </Col>
+        <Col>
+          <h3>Total=[{totalMarks}]</h3>
+        </Col>
+      </Row>
 
-    return (
-        <div className={"container"}>
-            <Row className={"col1 col3"} style={{textAlign: "center"}}>
-                <Col>
-                    <h3>{marksList.studentName}</h3>
-                </Col>
-                <Col>
-                    <h3>{marksList.examName}</h3>
-                </Col>
-                <Col>
-                    <h3>Total=[{totalMarks}]</h3>
-                </Col>
-            </Row>
-
-
-            {
-                marksList.studentAnswerSheet.map((question, index) => {
-
-                    return (
-                        <FacultyAssignRow
-                            key={question._id}
-                            index={index}
-                            question={question}
-                            handleMarksChange={handleMarksChange}
-                            user={user}
-                        />
-                    )
-                })
-            }
-            <Row className={" row row3"}>
-                <Col className={"col-md-12"}>
-                    {user === "Faculty" &&
-                    <button
-                        className={"saveBtn"}
-                        onClick={handleSaveMarks}>Save
-                    </button>
-                    }
-                </Col>
-            </Row>
-        </div>
-    )
+      {marksList.studentAnswerSheet.map((question, index) => {
+        return (
+          <FacultyAssignRow
+            key={question._id}
+            index={index}
+            question={question}
+            handleMarksChange={handleMarksChange}
+            user={user}
+          />
+        );
+      })}
+      <Row className={" row row3"}>
+        <Col className={"col-md-12"}>
+          {user === "Faculty" && (
+            <button className={"saveBtn"} onClick={handleSaveMarks}>
+              Save
+            </button>
+          )}
+        </Col>
+      </Row>
+    </div>
+  );
 }
 
 export default FacultyAssignMarks;
--- src/components/StudentQuestionPaperPage/StudentQuestionPaperPage.jsx
+++ src/components/StudentQuestionPaperPage/StudentQuestionPaperPage.jsx
@@ -1,14 +1,14 @@
-import React, {useContext, useState} from 'react';
-import {Col, Container, Row} from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useContext, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./StudentQuestionPaperPage.css";
-import {StudentQuestionAnswerContext} from "../../context/StudentQuestionAnswerContext";
-import quisysLogo from "../../images/quisysLogo.png"
+import { StudentQuestionAnswerContext } from "../../context/StudentQuestionAnswerContext";
+import quisysLogo from "../../images/quisysLogo.png";
 import QuestionAnswerSection from "./QuestionAnswerSection/QuestionAnswerSection";
-import {StudentDetailsContext} from "../../context/StudentDetailsContext";
-import {SelectedQuizContext} from "../../context/SelectedQuizContext(Student)";
-import {StudentAnswersheetContext} from "../../context/StudentAnswersheetContext";
-import {useHistory} from "react-router-dom";
+import { StudentDetailsContext } from "../../context/StudentDetailsContext";
+import { SelectedQuizContext } from "../../context/SelectedQuizContext(Student)";
+import { StudentAnswersheetContext } from "../../context/StudentAnswersheetContext";
+import { useHistory } from "react-router-dom";
 import Timer from "./Timer/Timer";
 
 const axios = require("axios").default;
@@ -16,133 +16,143 @@ const axios = require("axios").default;
 const baseUrl = "http://localhost:3000";
 
 function StudentQuestionPaperPage() {
+  // Only Question Answers
+  const [questionAnswer, setQuestionAnswer] = useContext(
+    StudentQuestionAnswerContext
+  );
 
-    // Only Question Answers
-    const [questionAnswer, setQuestionAnswer] = useContext(StudentQuestionAnswerContext);
+  const [currentQuestionNumber, setCurrentQuestionNumber] = useState(0);
 
-    const [currentQuestionNumber, setCurrentQuestionNumber] = useState(0);
+  // All Details about Current Quiz
+  const [currentQuizDetails, setCurrentQuizDetails] =
+    useContext(SelectedQuizContext);
 
-    // All Details about Current Quiz
-    const [currentQuizDetails, setCurrentQuizDetails] = useContext(SelectedQuizContext)
+  // Current Student Details
+  const [currentStudentDetails, setCurrentStudentDetails] = useContext(
+    StudentDetailsContext
+  );
 
-    // Current Student Details
-    const [currentStudentDetails, setCurrentStudentDetails] = useContext(StudentDetailsContext);
+  const [studentAnswerSheet, setStudentAnswerSheet] = useContext(
+    StudentAnswersheetContext
+  );
 
-    const [studentAnswerSheet, setStudentAnswerSheet] = useContext(StudentAnswersheetContext);
+  const questionArray = Array.from(
+    { length: questionAnswer.length },
+    (_, index) => index + 1
+  );
 
-    const questionArray = Array.from({length: questionAnswer.length}, (_, index) => index + 1);
+  const history = useHistory();
 
-    const history = useHistory();
-
-
-    function handleNext() {
-        if (currentQuestionNumber < (questionAnswer.length - 1)) {
-            setCurrentQuestionNumber(prevState => {
-                return (prevState + 1);
-            })
-        }
+  function handleNext() {
+    if (currentQuestionNumber < questionAnswer.length - 1) {
+      setCurrentQuestionNumber((prevState) => {
+        return prevState + 1;
+      });
     }
+  }
 
-    function handleSubmit() {
-        axios
-            .post(baseUrl + "/submitExam", studentAnswerSheet)
-            .then(function (response) {
-                console.log(response.data)
-                history.push("/StudentHomePage");
+  function handleSubmit() {
+    axios
+      .post(baseUrl + "/submitExam", studentAnswerSheet)
+      .then(function (response) {
+        console.log(response.data);
+        history.push("/StudentHomePage");
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }
 
-            })
-            .catch(function (error) {
-                console.log(error);
-            });
+  function handlePrevious() {
+    if (currentQuestionNumber > 0) {
+      setCurrentQuestionNumber((prevState) => {
+        return prevState - 1;
+      });
     }
+  }
 
-    function handlePrevious() {
-        if (currentQuestionNumber > 0) {
-            setCurrentQuestionNumber(prevState => {
-                return (prevState - 1);
-            })
-        }
-    }
+  function goToQuestion(event) {
+    const value = event.target.id;
+    let qNo = value - 1;
+    setCurrentQuestionNumber(qNo);
+  }
 
-    function goToQuestion(event) {
-        const value = event.target.id;
-        let qNo = value - 1;
-        setCurrentQuestionNumber(qNo)
-    }
+  return (
+    <Container className={"PageContainer"}>
+      <Row className={"NavBarContainer"}>
+        <Col style={{ textAlign: "left" }}>
+          <img className={"quisysLogo"} src={quisysLogo} alt={"quisys-logo"} />
+        </Col>
+        <Col style={{ textAlign: "center" }}>
+          <p className={"quizName"}>{currentQuizDetails.examName}</p>
+        </Col>
+        <Col style={{ textAlign: "right" }}>
+          <p className={"userDetails"}>
+            {currentStudentDetails.name}
+            <br />
+            {currentStudentDetails.email}
+          </p>
+        </Col>
+      </Row>
+      <Row className={"MainContainer"}>
+        <Col md={3} className={"TimerQuestionNumber"}>
+          <Row>
+            <Timer handleSubmitAction={handleSubmit} />
+          </Row>
+          <Row className={"questionNumbersContainer"}>
+            <Col style={{ textAlign: "left" }}>
+              {questionArray.map((number) => {
+                return (
+                  <button
+                    key={number}
+                    id={number}
+                    className={"questionNumbers"}
+                    onClick={goToQuestion}
+                  >
+                    {number}
+                  </button>
+                );
+              })}
+            </Col>
+          </Row>
+        </Col>
 
-
-    return (
-        <Container className={"PageContainer"}>
-            <Row className={"NavBarContainer"}>
-                <Col style={{textAlign: "left"}}>
-                    <img className={"quisysLogo"} src={quisysLogo} alt={"quisys-logo"}/>
+        <Col className="QuestionOptionContainer" md={9}>
+          <Row>
+            <Col style={{ textAlign: "left" }}>
+              <h4>Question No- {currentQuestionNumber + 1}</h4>
+              <QuestionAnswerSection
+                questionNo={currentQuestionNumber}
+                question={questionAnswer[currentQuestionNumber].question}
+                options={questionAnswer[currentQuestionNumber].options}
+                isTextField={questionAnswer[currentQuestionNumber].isText}
+              />
+            </Col>
+          </Row>
+          <Row className={"ButtonContainer"}>
+            <Col>
+              <Row>
+                <Col>
+                  <button onClick={handlePrevious}>Previous</button>
                 </Col>
-                <Col style={{textAlign: "center"}}>
-                    <p className={"quizName"}>{currentQuizDetails.examName}</p>
+                <Col>
+                  {currentQuestionNumber !== questionAnswer.length - 1 ? (
+                    <button onClick={handleNext} id={"next"}>
+                      Save & Next
+                    </button>
+                  ) : (
+                    <button onClick={handleSubmit} id={"next"}>
+                      Submit
+                    </button>
+                  )}
                 </Col>
-                <Col style={{textAlign: "right"}}>
-                    <p className={"userDetails"}>{currentStudentDetails.name}<br/>{currentStudentDetails.email}</p>
-                </Col>
-
-            </Row>
-            <Row className={"MainContainer"}>
-                <Col md={3} className={"TimerQuestionNumber"}>
-                    <Row>
-                        <Timer
-                            handleSubmitAction={handleSubmit}/>
-                    </Row>
-                    <Row className={"questionNumbersContainer"}>
-                        <Col style={{textAlign: "left"}}>{questionArray.map(number => {
-                                return (<button
-                                        key={number}
-                                        id={number}
-                                        className={"questionNumbers"}
-                                        onClick={goToQuestion}
-                                    >
-                                        {number}
-                                    </button>
-                                )
-                            }
-                        )
-                        }
-                        </Col>
-                    </Row>
-                </Col>
-
-                <Col className="QuestionOptionContainer" md={9}>
-
-                    <Row>
-                        <Col style={{textAlign: "left"}}>
-                            <h4>Question No- {currentQuestionNumber + 1}</h4>
-                            <QuestionAnswerSection
-                                questionNo={currentQuestionNumber}
-                                question={questionAnswer[currentQuestionNumber].question}
-                                options={questionAnswer[currentQuestionNumber].options}
-                                isTextField={questionAnswer[currentQuestionNumber].isText}
-                            />
-                        </Col>
-                    </Row>
-                    <Row className={"ButtonContainer"}>
-                        <Col>
-                            <Row>
-                                <Col>
-                                    <button onClick={handlePrevious}>Previous</button>
-                                </Col>
-                                <Col>
-                                    {currentQuestionNumber !== (questionAnswer.length - 1) ?
-                                        <button onClick={handleNext} id={"next"}>Save & Next</button> :
-                                        <button onClick={handleSubmit} id={"next"}>Submit</button>
-                                    }
-
-                                </Col>
-                            </Row>
-                        </Col>
-                    </Row>
-                </Col>
-            </Row>
-        </Container>
-    );
-
+              </Row>
+            </Col>
+          </Row>
+        </Col>
+      </Row>
+    </Container>
+  );
 }
 
 export default StudentQuestionPaperPage;
